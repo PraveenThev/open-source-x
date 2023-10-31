b6cb8598b1453dfc9d04add59aa5100ef34cfcad
Praveendev <97358239+PraveenDev111@users.noreply.github.com>
2023-10-31 22:55:47 +0530

    Add files via upload

---
+++ appbar.dart
@@ -0,0 +1,95 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../view/notification.dart';
+import 'settingspage.dart';
+
+class Appbar extends StatefulWidget implements PreferredSizeWidget {
+  final String title;
+
+  const Appbar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<Appbar> createState() => _AppbarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AppbarState extends State<Appbar> {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  late bool unreadNoti;
+
+  Future<void> checkNotifications() async {
+    QuerySnapshot qs = await FirebaseFirestore.instance
+        .collection('notifications')
+        .where('uid', isEqualTo: currentUser!.uid)
+        .where('readstatus', isEqualTo: false)
+        .get();
+    if (qs.docs.isNotEmpty) {
+      print(qs.docs);
+
+      setState(() {
+        unreadNoti = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PreferredSize(
+      preferredSize: widget.preferredSize,
+      child: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu_sharp,
+            size: 30,
+          ),
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+        ),
+        title: Text(
+          widget.title,
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications_none_sharp, size: 30),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const FlutterNotification(),
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              size: 30,
+            ),
+            onPressed: () {
+              // Open the settings page.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
